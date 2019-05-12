52102c9ea6a9d5ee28f81940a85ca99767f6073c
chiefsmurph <chiefsmurph@gmail.com>
2019-05-12 19:47:17 -0400

    YES

--- scans/accumulation.js
+++ scans/accumulation.js
@@ -109,7 +109,7 @@ module.exports = async (count = COUNT, collectionStr = 'all') => {
 
     const accBreakdowns = {
       inifinity: score => score === Number.POSITIVE_INFINITY,
-      gt300: score => score > 300,
+      gt300: score => score > 300 && score !== Number.POSITIVE_INFINITY,
       lt25: score => score < 25
     };
 
