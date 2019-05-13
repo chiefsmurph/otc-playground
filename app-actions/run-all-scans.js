c0f5fcf7d75071cf3cdbcc35d92cb1aa61505915
chiefsmurph <chiefsmurph@gmail.com>
2019-05-13 16:00:51 -0400

    ok

--- app-actions/run-all-scans.js
+++ app-actions/run-all-scans.js
@@ -1,15 +1,17 @@
 const scan = require('./scan');
 
 module.exports = async () => {
-  const scansToRun = [
-    'day-streaks',
-    'accumulation',
-    'ihub'
-  ];
+  const scansToRun = {
+    'day-streaks': true,
+    'accumulation': true,
+    'ihub': true,
+    'twitter': [undefined, null]  // not based on collection
+  };
   console.log({ scansToRun });
-  for (let scanName of scansToRun) {
+  for (let scanName of Object.keys(scansToRun)) {
     console.log('starting scan...', scanName);
-    await scan(scanName);
+    const additionalArgs = Array.isArray(scansToRun[scanName]) ? scansToRun[scanName] : [];
+    await scan(scanName, ...additionalArgs);
     console.log('finished scan...', scanName);
   }
 };
