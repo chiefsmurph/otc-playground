3ada25080303aa31368e87aadf40efbd171a382b
chiefsmurph <chiefsmurph@gmail.com>
2019-05-16 17:33:15 -0400

    aight

--- helpers/on-historical-change.js
+++ helpers/on-historical-change.js
@@ -1,18 +1,22 @@
 
 const getMostRecentHistoricalDate = require('../cli/get-most-recent-historical-date');
+const regCronAfter630 = require('../helpers/reg-cron-after-630');
 
 const fns = [];
 const onHistoricalChange = fnToRun => {
   let prevVal;
   fns.push(fnToRun);
-  setInterval(async () => {
+
+  const checkForChange = async () => {
     const curDateStr = await getMostRecentHistoricalDate();
     console.log(`most recent hist dateStr: ${curDateStr}`)
     if (prevVal && prevVal != curDateStr) {
       fns.forEach(fn => fn());
     }
     prevVal = curDateStr;
-  }, 1000 * 60 * 10);
+  };
+  checkForChange();
+  setInterval(checkForChange, 1000 * 60 * 10);
 };
 
 module.exports = onHistoricalChange;
