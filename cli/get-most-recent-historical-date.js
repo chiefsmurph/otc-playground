e4337225c281961f5cf8db363332c026879b1621
chiefsmurph <chiefsmurph@gmail.com>
2019-05-16 21:12:10 -0400

    questlove

--- cli/get-most-recent-historical-date.js
+++ cli/get-most-recent-historical-date.js
@@ -1,10 +1,16 @@
 const { getHistoricals } = require('../scraping-actions/get-historicals');
 const getDateStr = require('../helpers/get-datestr');
 
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
 module.exports = async (ticker = 'AAPL') => {
   const hists = await getHistoricals(ticker);
   const mostRecent = hists[0];
-  const dateStr = getDateStr(mostRecent.date);
-  // console.log(dateStr);
+  const date = mostRecent.date;
+  const dateStr = getDateStr(addDays(date, 1));
   return dateStr;
 };
